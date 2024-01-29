3e1bd6a8552b4ce4b01496c7a333e1c6c1334c32
Mukul2301 <mukulshende31@gmail.com>
2024-01-29 12:43:24 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import logo from "./currency.gif";
+import axios from "axios";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [exchange, setExchange] = useState({});
+  const [convertedAmount, setConvertedAmount] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = `https://v6.exchangerate-api.com/v6/697b1e5fff2b967f6930fa5b/latest/${fromCurrency}`;
+    axios
+      .get(apiUrl)
+      .then((res) => {
+        console.log(res);
+        setExchange(res.data.conversion_rates);
+      })
+      .catch((err) => {
+        console.log("Error on fetching", err);
+      });
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    const conversionRate = exchange[toCurrency];
+    if (conversionRate) {
+      const converted = amount * conversionRate;
+      setConvertedAmount(converted.toFixed(2));
+    }
+  }, [amount, fromCurrency, toCurrency, exchange]);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "amount":
+        setAmount(value);
+        break;
+      case "fromCurrency":
+        setFromCurrency(value);
+        break;
+      case "toCurrency":
+        setToCurrency(value);
+        break;
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        style={{ backgroundColor: "white" }}
+        src={logo}
+        width={90}
+        height={90}
+        alt=""
+      />
+      <h1>Currency Converter</h1>
+      <div className="exchange">
+        <div className="input">
+          <label className="inputLabel">Amount:</label>
+          <input
+            className="inputField"
+            name="amount"
+            type="number"
+            value={amount}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input">
+          <label className="inputLabel">From Currency:</label>
+
+          <select
+            name="fromCurrency"
+            className="inputField"
+            value={fromCurrency}
+            onChange={handleChange}
+          >
+            {Object.keys(exchange).map((currency) => (
+              <option className="options" key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="input">
+          <label className="inputLabel">To Currency:</label>
+
+          <select
+            name="toCurrency"
+            className="inputField"
+            value={toCurrency}
+            onChange={handleChange}
+          >
+            {Object.keys(exchange).map((currency) => (
+              <option className="options" key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="output">
+        <h2>
+          Converted Amount:
+          <b className="boldResult">
+            {convertedAmount} {toCurrency}
+          </b>
+        </h2>
+      </div>
     </div>
   );
 }
